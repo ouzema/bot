023ac4c7aafff5c335e3180f5a903723cea4c905
ouzema <ouzema@yahoo.com>
2023-11-23 23:24:46 +0100

    first commit

---
+++ utils/constants.py
@@ -0,0 +1,45 @@
+chat = {
+    "TITLE1": "About Me",
+    "TITLE2": "AI Chat",
+    "menu_v": {
+   "container": {"background-color": "#a7c5ff"},
+   "icon": {"color": "white", "font-size": "25px"}, 
+   "nav-link": {"font-size": "25px", "text-align": "center", "color": "white", "margin":"0px", "--hover-color": "#1b97ff"},
+   "nav-link-selected": {"background-color": "#1b97ff"}
+    },
+    "menu_h": {
+        "container": {"padding": "0px",
+                      "display": "grid",
+                      "margin": "0!important",
+                      "background-color": "#212121"
+                      },
+        "icon": {"color": "#bd93f9", "font-size": "14px"},
+        "nav-link": {
+            "font-size": "14px",
+            "text-align": "center",
+            "margin": "auto",
+            "background-color": "#262626",
+            "height": "30px",
+            "width": "13rem",
+            "color": "#edff85",
+            "border-radius": "5px"
+        },
+        "nav-link-selected": {
+            "background-color": "#212121",
+            "font-weight": "300",
+            "color": "#f7f8f2",
+            "border": "1px solid #e55381"
+        }
+    }
+}
+
+info = {
+   "Pronoun": "him", 
+   "Name": "Oussema",
+   "Full_Name": "Mohamed Oussema Laribi",
+   "Intro": "A Data Scientist and AI Enthusiast",
+   "About":"Hey there, I'm Oussema! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business.",
+   "City":"Sousse, Tunisia",
+   "Photo":"""<a href=\"https://www.linkedin.com/in/ouzema/\"><img src=\"https://drive.google.com/file/d/1JWo4fSOZ4sA3Fk5hMMM1focwL8Dy_9R9/view?usp=sharing" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "aribi.mohamedoussama@episousse.com.tn"
+}
